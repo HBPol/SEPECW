4e25ea06b47770ef3938b6d1a1d64af23a862d7b
16069344 <16069344@kubuntu>
2018-04-20 00:14:14 +0100

    File in resources/views/layouts/app.blade.php edited to be used as master blade template.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,75 +1,49 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- Style for this page in particular -->
+    <style type="text/css">
+    body {
+    	padding-top: 3.5rem; /* Move down content because we have a fixed navbar that is 3.5rem tall */
+    	background-color: #fff;
+    	color: #636b6f;
+    	font-family: 'Raleway', sans-serif;
+    	font-weight: 100;
+    	height: 100vh;
+    	margin: 0;
+    }
+    </style>
+    
+    <title></title>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+	
+    @include ('layouts/topnav')
+	<div class="container">
+    	@if(Session::has('flash_message'))
+            <div class="container">      
+                <div class="alert alert-success"><em> {!! session('flash_message') !!}</em>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        @endif 
+        
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">              
+            	@include ('errors.list') {{-- Including error file --}}
+            </div>
+        </div>
+        <br>
+        @yield('content')
+        <br>
     </div>
+    @include ('layouts/footer')
+    
 </body>
 </html>
---
+++ resources/views/layouts/topnav.blade.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbars_01">
+        	<ul class="navbar-nav mr-auto">
+        		<li class="nav-item active"><a class="nav-link" href="#"><i class="fas fa-home"></i></a></li>
+        		<!-- <li class="nav-item"><a class="nav-link" href="#">Drawings</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Sculpture</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Photography</a></li>  -->				
+        	</ul>
+        	
+        	<form class="form-inline my-2 my-lg-0" action="">
+        		<input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
+        		<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+        	</form>
+                		
+						@guest
+						
+                            <a href="{{ route('login') }}" class="btn btn-outline-secondary ml-2 my-2 my-sm-0"><i class="fas fa-sign-in-alt"></i>{{ __('Login') }}</a>
+                            <a href="{{ route('register') }}" class="btn btn-outline-primary ml-2 my-2 my-sm-0">{{ __('Register') }}</a>
+
+                        @else
+    						<ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle ml-2 my-2 my-sm-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                	<i class="fas fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    								
+    								<a class="dropdown-item" href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
+    								<a class="dropdown-item" href="#">Another item</a>
+    								<div class="dropdown-divider"></div>
+    								<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>&nbsp;{{ __('Logout') }}</a>
+    								<a class="dropdown-item" href="#">Something else here</a>
+    								
+                                </div>
+    						</li>
+    						
+    						</ul>
+    						
+    						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+    						</form>
+                               
+                        @endguest
+            
+        </div>
+    </div>
+</nav>
